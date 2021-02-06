70e4d5bec2dd33ff90573bc53c044e76444a469e
Reza <development.rezasajadi@gmail.com>
2021-02-06 22:43:39 +0330

    Adding to the Application.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
-import MainScreen from "./screens/mainScreen/MainScreen";
+import React, { Component } from "react";
 
-function App() {
-  return <MainScreen />;
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import GitUserRepositories from "./dataStore/DataStore";
+import UserRepositoryConnector from "./components/UserRepositoryConnector";
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={GitUserRepositories}>
+        <Router>
+          <Switch>
+            <Route path="/shop" component={UserRepositoryConnector} />
+            <Redirect to="/shop" />
+          </Switch>
+        </Router>
+      </Provider>
+    );
+  }
 }
-
-export default App;
